73585aece6a61e74e20ff6ec9fcdce329249297e
bima1313 <bimairfy13@gmail.com>
2026-01-22 00:28:57 +0700

    feat(logic): implement get restaurant api

--- apps/merchant/features/restaurants/service/getRestaurantService.ts
+++ apps/merchant/features/restaurants/service/getRestaurantService.ts
@@ -1,5 +1,5 @@
 import { webResponse } from "@/schema/webResponse";
-import { restaurants } from "../models/restaurant";
+import { restaurant, restaurants } from "../models/restaurant";
 import { cookies } from "next/headers";
 
 const baseUrl = process.env.NEXT_PUBLIC_MERCHANTS_API_URL;
@@ -23,4 +23,28 @@ export const getRestaurantsService = async () => {
   const result = await response.json() as webResponse<restaurants>;  
 
   return result;
+};
+
+export const getRestaurantService = async (restaurantId: string) => {
+  const cookieData = await cookies();
+  const token = cookieData.get("token");  
+
+  if (!token) {
+    throw new Error("Session has expired, please log in again.");
+  }
+
+  const response = await fetch(`${baseUrl}/restaurants/${restaurantId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token.value}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const result = await response.json();  
+
+return {
+    data: result.data.restaurant, // Masuk ke level terdalam
+    error: result.error || null
+  } as webResponse<restaurant>;
 };
